6676e2e25bfef97ad0b8905d0dd521f875eaf5df
zyqifly <zyqifly@hotmail.com>
2020-08-27 14:45:35 +0800

    VS2013下CMACK工程显示没问题； <BUG>使用VS2013 控制台程序加载相机库，显示的图像是扭曲的。

--- CameraFactorSDK/include/SDKImport.h
+++ CameraFactorSDK/include/SDKImport.h
@@ -2,7 +2,9 @@
 #pragma warning(disable:4251)
 #pragma warning(disable:26451)
 #ifdef CAMERA_FACTORY_DYNAMIC
+#pragma message("__declspec(dllexport)")	
 #define	CAMERA_FACTORY_API	__declspec(dllexport)
-#else	
+#else
+#pragma message("not __declspec(dllexport)")	
 #define	CAMERA_FACTORY_API
 #endif // CAMERA_FACTORY_DYNAMIC
